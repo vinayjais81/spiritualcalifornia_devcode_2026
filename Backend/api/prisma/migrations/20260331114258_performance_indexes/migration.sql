387ded4b8c41f5c6e969b505fa24fc36fe047620
vinayjais81 <vinayjais81@gmail.com>
2026-04-09 17:20:53 +0530

    Make performance_indexes migration idempotent; untrack env.local

--- Backend/api/prisma/migrations/20260331114258_performance_indexes/migration.sql
+++ Backend/api/prisma/migrations/20260331114258_performance_indexes/migration.sql
@@ -1,35 +1,35 @@
 -- DropIndex
-DROP INDEX "blog_posts_published_date_idx";
+DROP INDEX IF EXISTS "blog_posts_published_date_idx";
 
 -- DropIndex
-DROP INDEX "bookings_status_idx";
+DROP INDEX IF EXISTS "bookings_status_idx";
 
 -- DropIndex
-DROP INDEX "carts_updated_idx";
+DROP INDEX IF EXISTS "carts_updated_idx";
 
 -- DropIndex
-DROP INDEX "events_upcoming_idx";
+DROP INDEX IF EXISTS "events_upcoming_idx";
 
 -- DropIndex
-DROP INDEX "guide_profiles_published_rating_idx";
+DROP INDEX IF EXISTS "guide_profiles_published_rating_idx";
 
 -- DropIndex
-DROP INDEX "notifications_user_unread_idx";
+DROP INDEX IF EXISTS "notifications_user_unread_idx";
 
 -- DropIndex
-DROP INDEX "orders_seeker_status_idx";
+DROP INDEX IF EXISTS "orders_seeker_status_idx";
 
 -- DropIndex
-DROP INDEX "payments_stripe_status_idx";
+DROP INDEX IF EXISTS "payments_stripe_status_idx";
 
 -- DropIndex
-DROP INDEX "products_active_created_idx";
+DROP INDEX IF EXISTS "products_active_created_idx";
 
 -- DropIndex
-DROP INDEX "reviews_target_approved_idx";
+DROP INDEX IF EXISTS "reviews_target_approved_idx";
 
 -- DropIndex
-DROP INDEX "soul_tours_upcoming_idx";
+DROP INDEX IF EXISTS "soul_tours_upcoming_idx";
 
 -- DropIndex
-DROP INDEX "tour_bookings_status_idx";
+DROP INDEX IF EXISTS "tour_bookings_status_idx";
